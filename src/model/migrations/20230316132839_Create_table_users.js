346d5f48e3029d1cf28221a1e8e98b0fd26bcd05
WAGNER SOUZA <wagner.souza20@fatec.sp.gov.br>
2023-03-16 10:54:34 -0300

    Users Migration Created

---
+++ src/model/migrations/20230316132839_Create_table_users.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable("users", (table) =>{
+    table.increments("id").primary();
+    table.string("email").unique({indexName:"users_unique_email"}).notNullable();
+    table.string("senha").notNullable();
+    table.string("telefone",20).notNullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable("users");
+};
